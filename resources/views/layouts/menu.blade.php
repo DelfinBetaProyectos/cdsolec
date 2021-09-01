1be4155fbecc5367fdce9f45276a290b2e301499
Romulo Martinez <mromulo07@gmail.com>
2021-09-01 05:15:18 -0430

    Categorias principales añadidas al menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -21,104 +21,37 @@
                 <div x-data="tabsMegaMenu()">
                   <ul class="flex justify-start items-center">
                     <template x-for="(tab, index) in tabs" :key="index">
-                      <li class="cursor-pointer py-2 px-4 text-cdsolec-green-dark border-b-4 border-gray-400"
-                        :class="activeTab===index ? 'text-cdsolec-green-dark border-cdsolec-green-dark' : ''" @click="activeTab = index" x-text="tab"></li>
+                      <li class="cursor-pointer py-2 px-4 text-cdsolec-green-dark border-b-4 border-gray-400" :class="activeTab===index ? 'text-cdsolec-green-dark border-cdsolec-green-dark' : ''" @click="activeTab = index" x-text="tab"></li>
                     </template>
                   </ul>
-              
+                  
+                  <!-- obtener datos desde la API-->
+                  @php
+
+                  $ch = curl_init();
+
+                  curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Content-Type:application/json', 'DOLAPIKEY: bJD33zn72gC9O6duXc59vOZh2N8OFiFk' ) );
+                  curl_setopt($ch, CURLOPT_URL, 'http://www.cd-solec.com/erp/htdocs/api/index.php/categories?sortfield=t.rowid&sortorder=ASC&limit=100&sqlfilters=fk_parent%3D0');
+                  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                  curl_setopt($ch, CURLOPT_HEADER, 0);
+                  $data = json_decode(curl_exec($ch),true);
+
+                  curl_close($ch);
+
+                  @endphp
+
                   <div class="pt-4 border-t border-gray-400">
                     <div x-show="activeTab===0">
                       <div class="grid gap-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                        @foreach($data as $item)
                         <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
                           <span class="fas fa-angle-right mr-1"></span>
-                          Distribución de la energía
+                          {{ $item['label']}}
                         </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Controles industriales
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Automatización industrial
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Envolventes
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Redes de datos y comunicación
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Gestión y organización de cableado
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Canalizaciones eléctricas, data y comunicaciones
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Conductores eléctricos de baja tensión
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Calidad de energía
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Fuentes de energía
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Distribución de energía M.T.
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Iluminación tecnología LED
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Tomacorrientes e interruptores terminales
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Instrumentación electrónica
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Sistemas de control y actuación neumáticos
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Sistemas de inmotica y domotica. Soluciones de Building Automation
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Seguridad y control de acceso
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Detección y extinsion de incendio
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Seguridad industrial
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Soluciones integrales
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Soluciones para medios de fijación
-                        </a>
-                        <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
-                          <span class="fas fa-angle-right mr-1"></span>
-                          Herramientas
-                        </a>
+                        @endforeach
                       </div>
                     </div>
+
                     <div x-show="activeTab===1">
                       <div class="grid gap-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                         <a href="#" class="block mr-4 px-2 hover:bg-cdsolec-green-light">
@@ -240,11 +173,10 @@
             <div x-data="tabsMegaMenu()">
               <ul class="flex justify-start items-center">
                 <template x-for="(tab, index) in tabs" :key="index">
-                  <li class="cursor-pointer py-2 px-4 text-cdsolec-green-dark border-b-4 border-gray-400"
-                    :class="activeTab===index ? 'text-cdsolec-green-dark border-cdsolec-green-dark' : ''" @click="activeTab = index" x-text="tab"></li>
+                  <li class="cursor-pointer py-2 px-4 text-cdsolec-green-dark border-b-4 border-gray-400" :class="activeTab===index ? 'text-cdsolec-green-dark border-cdsolec-green-dark' : ''" @click="activeTab = index" x-text="tab"></li>
                 </template>
               </ul>
-          
+
               <div class="pt-4 border-t border-gray-400">
                 <div x-show="activeTab===0">
                   <div class="grid gap-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
@@ -422,7 +354,7 @@
     <!-- Navigation Links Right -->
     <div class="border-b border-cdsolec-green-dark">
       <x-jet-responsive-nav-link href="{{ route('cart') }}" :active="request()->routeIs('cart')">
-        <i class="fas fa-fw mr-1 fa-shopping-cart"></i> 
+        <i class="fas fa-fw mr-1 fa-shopping-cart"></i>
         <span class="relative">Compra
           <div class="absolute animate-bounce bg-cdsolec-green-dark rounded bottom-0 -right-8">
             <span class="px-2 text-white text-xs">0</span>
