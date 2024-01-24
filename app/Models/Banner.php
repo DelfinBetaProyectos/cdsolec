42c84b86c61e171cfcd308f5d9947bcb785d1658
Dayan Betancourt <dkbetancourt@gmail.com>
2024-01-24 10:00:26 -0400

    feat :bulb: Migration Banners and show Welcome

---
+++ app/Models/Banner.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Queries\QueryFilter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class Banner extends Model implements Auditable
+{
+	use HasFactory;
+	use SoftDeletes;
+	use \OwenIt\Auditing\Auditable;
+  
+	/**
+	 * The attributes that are mass assignable.
+	 * 
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'image'
+	];
+
+	/**
+	 * Get the url image.
+	 *
+	 * @return string
+	 */
+	public function getUrlImageAttribute()
+	{
+		return 'img/'.$this->image;
+	}
+
+	/**
+	 * Search Filters.
+	 */
+	public function scopeFilterBy($query, QueryFilter $filters, array $data)
+	{
+		return $filters->applyTo($query, $data);
+	}
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
           SettingSeeder::class,
-          ContentSeeder::class
+          ContentSeeder::class,
+          BannerSeeder::class
         ]);
     }
 }
